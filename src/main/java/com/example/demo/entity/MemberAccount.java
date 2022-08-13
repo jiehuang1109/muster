48f51ad7d86235bc15effe8f57e29dc43acb6a25
SAMUEL\fongj <fongjie1109@gmail.com>
2022-08-14 03:06:08 +0800

    Initialize muster project

---
+++ src/main/java/com/example/demo/entity/MemberAccount.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.databind.deser.Deserializers;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberAccount extends Deserializers.Base {
+}
